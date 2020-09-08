3aae178e95b5bb561532fb4aaa972eee7ab4da42
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2020-09-08 20:10:17 +0000

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
       <h1>lets build Amazone store from clever programmer</h1>
+      <Header />
     </div>
   );
 }
