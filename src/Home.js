26db0e50839e749bcb46af845f3411833c4a34ff
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2020-09-08 21:44:53 +0000

    update Home.js

--- src/Home.js
+++ src/Home.js
@@ -3,8 +3,12 @@ import "./Home.css";
 
 function Home(){
     return (
-        <div className="home"></div>
-      )
+        <div className="home">
+        <div className="home__container">
+            <img src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE_XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt="">
+        </div>
+        </div>
+      );
 }
 
 export default Home
