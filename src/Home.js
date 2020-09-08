a64178c683dae4ceedfcebccd022ac415d8b912c
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2020-09-08 22:31:18 +0000

    update Home.js

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,9 @@ function Home(){
     return (
         <div className="home">
         <div className="home__container">
-            <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423592668_.jpg" alt=""
+            <img
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+          alt=""
             />
         </div>
         </div>
