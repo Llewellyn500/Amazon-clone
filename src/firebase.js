74eae0478119df6877f0cf8191c6011dc2ff0db6
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2020-09-08 19:16:15 +0000

    update firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBG4Dp0HHjCt435q7NSpe1AABJfhw8PXjE",
+  authDomain: "e-clone-f33a0.firebaseapp.com",
+  databaseURL: "https://e-clone-f33a0.firebaseio.com",
+  projectId: "e-clone-f33a0",
+  storageBucket: "e-clone-f33a0.appspot.com",
+  messagingSenderId: "567449758288",
+  appId: "1:567449758288:web:202964b9779e1bb6cdff55",
+  measurementId: "G-EXTJ85BWKW"
+};
