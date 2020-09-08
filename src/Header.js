c595071ce37fc887368d42dc41127d6743fffc9b
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2020-09-08 20:08:18 +0000

    update Header.js

--- src/Header.js
+++ src/Header.js
@@ -13,7 +13,26 @@ function Header() {
           </div>
           
           <div className="header__nav">
-          
+            <div className="header__option">
+              <span 
+              className="header__optionLineOne">Hello guest</span>
+              <span 
+              className="header__optionLineTwo">Sign In</span>
+            </div>
+            
+            <div className="header__option">
+              <span 
+              className="header__optionLineOne">Returns</span>
+              <span 
+              className="header__optionLineTwo">& Orders</span>
+            </div>
+            
+            <div className="header__option">
+              <span 
+              className="header__optionLineOne">Your</span>
+              <span 
+              className="header__optionLineTwo">Prime</span>
+            </div>
           </div>
         </div>
       )
