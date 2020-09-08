07def4dc840ee75977813b88f4dce51bea6ed4b9
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2020-09-08 21:12:46 +0000

    update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css';
 import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
     return (
@@ -36,6 +37,10 @@ function Header() {
               className="header__optionLineTwo">Prime</span>
             </div>
             
+            <div className="header__optionBasket">
+                <ShoppingBasketIcon />
+                <span className="header__optionLineTwo header__basketCount">0</span>
+            </div>
             
           </div>
         </div>
