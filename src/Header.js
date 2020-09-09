d80ac01bb8a8bda32942978bf8c0e1709c9228a0
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2020-09-09 09:13:52 +0000

    update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,50 +1,55 @@
-import React from 'react';
-import './Header.css';
-import SearchIcon from "@material-ui/icons/Search";
+import React from "react";
+
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import SearchIcon from "@material-ui/icons/Search";
 
-function Header() {
-    return (
-        <div className='header'>
-          <img
-          className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-          />
-          
-          <div className="header__search">
-            <input className="header__searchInput" type="text" />
-              <SearchIcon className="header__searchIcon" />
-          </div>
-          
-          <div className="header__nav">
-            <div className="header__option">
-              <span 
-              className="header__optionLineOne">Hello guest</span>
-              <span 
-              className="header__optionLineTwo">Sign In</span>
-            </div>
-            
-            <div className="header__option">
-              <span 
-              className="header__optionLineOne">Returns</span>
-              <span 
-              className="header__optionLineTwo">& Orders</span>
-            </div>
-            
-            <div className="header__option">
-              <span 
-              className="header__optionLineOne">Your</span>
-              <span 
-              className="header__optionLineTwo">Prime</span>
-            </div>
-            
-            <div className="header__optionBasket">
-                <ShoppingBasketIcon />
-                <span className="header__optionLineTwo header__basketCount">0</span>
-            </div>
-            
-          </div>
+import "./Header.css";
+import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
+
+const Header = () => {
+  const [{ basket }, dispatch] = useStateValue();
+
+  return (
+    <div className="header">
+      <Link to="/">
+        <img
+          alt="headerLogo"
+          className="header__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        />
+      </Link>
+      <div className="header__search">
+        <input className="header__searchInput" type="text" />
+        <div className="header__searchIcon">
+          {" "}
+          <SearchIcon />
         </div>
-      )
-}
+      </div>
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Sami</span>
+          <span className="header__optionLineTwo">Sign In</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+        <Link to="/checkout">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__optionLineTwo header__basketCount">
+              {basket?.length}
+            </span>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
