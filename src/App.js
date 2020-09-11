39a3f206e1180339b2af2ada33a645bdeb9ecb04
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2020-09-11 10:37:09 +0000

    update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
+import Login from "./Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
