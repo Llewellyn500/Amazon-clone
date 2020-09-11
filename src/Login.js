58000f8dcbc3bf98581f412bb62b24b9fbf444a4
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2020-09-11 09:30:44 +0000

    update Login.js

---
+++ src/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Login.css';
+import { Link } from "react-router-dom";
+
+function Login() {
+  return (
+      <div className=''>
+      <Link to '/'>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png" />
+      </Link>
+      </div>
+    )
+}
+
+export default Login
