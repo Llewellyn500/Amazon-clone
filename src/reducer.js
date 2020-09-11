1035b3bb6ad9606965ef6b43e6a58565c5d606ba
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2020-09-11 11:44:29 +0000

    update reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -34,6 +34,12 @@ const reducer = (state, action) => {
             ...state,
             basket: newBasket
           }
+  
+        case "SET_USER";
+          return {
+            ...state,
+            user: action.user
+          }
 
     default:
       return state;
