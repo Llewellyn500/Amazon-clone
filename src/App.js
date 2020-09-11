31112c59e7d03d03b6d7e86e187b6f79dfa45419
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2020-09-11 11:42:54 +0000

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
 import Login from "./Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
+ 
 function App() {
+  const [{}, dispatch] = useStateValue();
+  
+  useEffect(() => {
+    auth.onAuthStateChanged(authUser => {
+      console.log('THE USER IS >>>', authUser);
+      
+      if (authUser) {
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      } else {
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+  },[])
+  
+  
   return (
     <Router>
       <div className="app">
