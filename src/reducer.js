6e31b9f39849729e0ee9bd463f86d01fe0037573
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2020-09-11 00:51:51 +0000

    Finished Day 2 HomeWork

--- src/reducer.js
+++ src/reducer.js
@@ -2,7 +2,7 @@ export const initialState = {
   basket: [],
 };
 
-  export const c =(basket) =>
+  export const getBasketTotal =(basket) =>
     basket?.reduce((amount, item) => item.price + amount, 0);
 
 const reducer = (state, action) => {
