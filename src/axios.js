d4bd72884167e96dde44010ee63863cc9e20825b
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2020-09-11 19:02:03 +0000

    update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,9 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   // THE API (cloud function) URL
-  baseURL: 'http://localhost:5001/e-clone-f33a0/us-central1/api'
-    // "http://localhost:5001/e-clone-f33a0/us-central1/api",
+  baseURL:  'https://us-central1-e-clone-f33a0.cloudfunctions.net/api'
+  //'http://localhost:5001/e-clone-f33a0/us-central1/api'
 });
 
 export default instance;
-
