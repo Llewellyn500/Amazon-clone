26d28e11574af6f176d24a6794cf30b22c6c8398
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2020-09-11 10:51:57 +0000

    update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBG4Dp0HHjCt435q7NSpe1AABJfhw8PXjE",
   authDomain: "e-clone-f33a0.firebaseapp.com",
@@ -8,3 +10,10 @@ const firebaseConfig = {
   appId: "1:567449758288:web:202964b9779e1bb6cdff55",
   measurementId: "G-EXTJ85BWKW"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth};
