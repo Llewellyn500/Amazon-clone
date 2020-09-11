34e84cb76d2aa2367c7a71a3810c5e8d7618b4dd
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2020-09-11 12:06:13 +0000

    update Checkout.js

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,7 +5,7 @@ import { useStateValue } from "./StateProvider";
 import CheckoutProduct from "./CheckoutProduct";
 
 function Checkout() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
   
   return (
     <div className="checkout">
@@ -18,6 +18,7 @@ function Checkout() {
         />
         
         <div>
+        <h3>Hello, {user?.email}</h3>
           <h2 className="checkout__title"> Your shopping Basket</h2>
           
           {basket.map(item => (
