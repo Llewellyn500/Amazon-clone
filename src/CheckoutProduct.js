4d08b43b4bf0f25946290f9003775caa4c33f931
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2020-09-11 08:45:38 +0000

    update CheckoutProduct.js

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,7 +1,17 @@
 import React from "react";
-import "./CheckoutProduct.css"
+import "./CheckoutProduct.css";
+import { useStateValue } from "./StateProvider";
 
 function CheckoutProduct({id, image, title, price, rating}) {
+  const [{ basket },dispatch] = useStateValue();
+  
+    const removeFromBasket = () => {
+      dispatch({
+        type: 'REMOVE_FROM_BASKET',
+        id: id,
+      })
+    }
+  
   return (
       <div className="checkoutProduct">
         <img className="checkoutProduct__image" src={image} />
@@ -19,7 +29,7 @@ function CheckoutProduct({id, image, title, price, rating}) {
           <p>🌟</p>
           ))}
         </div>
-        <button>Remove From Basket</button>
+        <button onClick={removeFromBasket}>Remove From Basket</button>
       </div>
     </div>
     )
