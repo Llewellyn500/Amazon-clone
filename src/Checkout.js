8e68a8b12716546b264a3b59a7f26073c64624cc
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2020-09-11 00:58:28 +0000

    update Checkout.js

--- src/Checkout.js
+++ src/Checkout.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Checkout.css";
 import Subtotal from "./Subtotal";
 
-const Checkout = () => {
+function Checkout() {
   return (
     <div className="checkout">
     
@@ -12,8 +12,10 @@ const Checkout = () => {
           className="checkout__ad"
           src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423592668_.jpg"
         />
+        
         <div>
           <h2 className="checkout__title"> Your shopping Basket</h2>
+          
           {/** BasketItem */}
            {/** BasketItem */}
             {/** BasketItem */}
