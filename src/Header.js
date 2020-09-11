78aaf2e24d1e7b27f05bb1cf5fd99a71ee1b2b14
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2020-09-11 11:55:41 +0000

    update Header.js

--- src/Header.js
+++ src/Header.js
@@ -6,9 +6,16 @@ import SearchIcon from "@material-ui/icons/Search";
 import "./Header.css";
 import { Link } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
 
 function Header() {
   const [{ basket }, dispatch] = useStateValue();
+  
+  const handleAuthentication = () => {
+    if (user) {
+      auth.signOut();
+    }
+  }
 
   return (
     <div className="header">
@@ -22,15 +29,14 @@ function Header() {
       <div className="header__search">
         <input className="header__searchInput" type="text" />
         <div className="header__searchIcon">
-          {" "}
           <SearchIcon />
         </div>
       </div>
       <div className="header__nav">
       <Link to='/login'>
-        <div className="header__option">
+        <div onClick={handleAuthentication} className="header__option">
           <span className="header__optionLineOne">Hello Guest</span>
-          <span className="header__optionLineTwo">Sign In</span>
+          <span className="header__optionLineTwo">{user ? 'Sign Out' : 'Sign In'}</span>
         </div>
         </Link>
         
