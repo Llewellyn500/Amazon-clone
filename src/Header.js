25024b77fb51ab637e0105932985152ccebe0e4f
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2020-09-11 00:33:48 +0000

    update Header.js

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ import "./Header.css";
 import { Link } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
 
-const Header = () => {
+function Header = () => {
   const [{ basket }, dispatch] = useStateValue();
 
   return (
